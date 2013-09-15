2e8cd038560591738876464acc96e3d84e5b8801
naominix <legal.h2@gmail.com>
2013-09-15 18:15:56 +0900

    Included TestProject

--- gen/R.java.d
+++ gen/R.java.d
@@ -12,8 +12,4 @@
 /Users/naotan/Documents/workspace/CountUp/res/values-sw720dp-land/dimens.xml \
 /Users/naotan/Documents/workspace/CountUp/res/values-v11/styles.xml \
 /Users/naotan/Documents/workspace/CountUp/res/values-v14/styles.xml \
-/Users/naotan/Documents/workspace/CountUp/bin/res/drawable-hdpi/ic_launcher.png \
-/Users/naotan/Documents/workspace/CountUp/bin/res/drawable-mdpi/ic_launcher.png \
-/Users/naotan/Documents/workspace/CountUp/bin/res/drawable-xhdpi/ic_launcher.png \
-/Users/naotan/Documents/workspace/CountUp/bin/res/drawable-xxhdpi/ic_launcher.png \
 /Users/naotan/Documents/workspace/CountUp/bin/AndroidManifest.xml \
